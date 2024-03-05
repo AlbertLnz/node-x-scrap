d5bc73e3325015659e5a8571c621a66094af9369
AlbertLnz <albert2000_lanza@hotmail.com>
2024-03-05 18:39:27 +0100

    Copy cURL request of user (@jh3yy) called 'UserTweets', paste it in terminal & get it the JSON (userTweets.json)

---
+++ requests/userTweetsCurl.js
@@ -0,0 +1,23 @@
+curl 'https://twitter.com/i/api/graphql/WwS-a6hAhqAAe-gItelmHA/UserTweets?variables=%7B%22userId%22%3A%222396811776%22%2C%22count%22%3A20%2C%22includePromotedContent%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22c9s_tweet_anatomy_moderator_badge_enabled%22%3Atrue%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Atrue%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22rweb_video_timestamps_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D' \
+  -H 'authority: twitter.com' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA' \
+  -H 'content-type: application/json' \
+  -H 'cookie: guest_id=v1%3A170888751300788505; d_prefs=MjoxLGNvbnNlbnRfdmVyc2lvbjoyLHRleHRfdmVyc2lvbjoxMDAw; gt=1765066590142472527; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCN9JqA%252BOAToMY3NyZl9p%250AZCIlZTE3MTQyMGRiMWNlMjBiYWE2NTBhYmRhNTEzOTMyZWQ6B2lkIiU5MmMx%250AZGZmNjY2YTI3MDJmNTk1MWM1MjA0MGYyNzRhZg%253D%253D--5d6afa3124441777bdb716910ad8765e4ee96b3b; kdt=afqdQ32JEqfUn1BJC5TFJDydPHBlG3ZU9mTDp8uR; auth_token=07cc17e9ec7c590bedeaeede79b0631f9a1b809e; ct0=8aa3a3989ed83621ff0b98152e8d24960398d27a59ecde07cb0b80b44fe6ff0cf7f68cb4600676c4cb146c5f9843caab883a439b17b88e47af54a06b26efa79e171194d8a25ddd0c898d813465c2e282; att=1-vYagwZRDEsvztXYlYOmyXwEPrDAiWJipvkJr3UeU; lang=es; twid=u%3D1453133694567428097' \
+  -H 'referer: https://twitter.com/jh3yy' \
+  -H 'sec-ch-ua: "Not A(Brand";v="99", "Brave";v="121", "Chromium";v="121"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-gpc: 1' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36' \
+  -H 'x-client-transaction-id: RSLbqL51W9sN5z5qTEIEGaLOV0zZuomRxeVmF+UHuIe2txYU/GnMm6XbP8SVbjDRQmm20kTOM+2hE1CYnB9MjNZM4+gaRA' \
+  -H 'x-client-uuid: 1d5e4872-0913-46bf-a111-78e1c1bb142f' \
+  -H 'x-csrf-token: 8aa3a3989ed83621ff0b98152e8d24960398d27a59ecde07cb0b80b44fe6ff0cf7f68cb4600676c4cb146c5f9843caab883a439b17b88e47af54a06b26efa79e171194d8a25ddd0c898d813465c2e282' \
+  -H 'x-twitter-active-user: yes' \
+  -H 'x-twitter-auth-type: OAuth2Session' \
+  -H 'x-twitter-client-language: es' \
+  --compressed
